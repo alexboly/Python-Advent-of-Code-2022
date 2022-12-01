9bc58352724a523592d315ef2a9456159cf340bd
Alex Bolboaca <alexboly@gmail.com>
2022-12-01 15:47:56 +0200

    Read from file

--- Dec-1/calories-tests.py
+++ Dec-1/calories-tests.py
@@ -1,6 +1,6 @@
 import unittest
 from parameterized import parameterized, parameterized_class
-from elfCalories import bestElf
+from elfCalories import bestElf, caloriesTextToList
 
 class TestElfCalories(unittest.TestCase):
 
@@ -10,5 +10,18 @@ class TestElfCalories(unittest.TestCase):
         ("first has two values", [[1, 2], [2]], 3),
         ])
     def test_calories(self, name, input, expected):
-        self.assertEquals(bestElf(input), expected)
+        self.assertEqual(bestElf(input), expected)
 
+
+    @parameterized.expand([
+        ("single value", """1""", [[1]]),
+        ("single different value", """2""", [[2]]),
+        ("two values", """1
+2""", [[1, 2]]),
+        ("first has two values", """1
+2
+
+3""", [[1, 2], [3]]),
+        ])
+    def test_fromCaloriesTextToList(self, name, input, expected):
+        self.assertEqual(caloriesTextToList(input), expected)
--- Dec-1/elfCalories.py
+++ Dec-1/elfCalories.py
@@ -1,9 +1,22 @@
+import itertools
+
 def bestElf(calories):
     totalCaloriesPerElf = [sum(elfCalories) for elfCalories in calories]
     return max(totalCaloriesPerElf)
 
+
+def convertLinesToListOfInts(lines):
+    return map(int, lines.splitlines())
+
+def caloriesTextToList(caloriesText):
+    caloriesPerElfStrings = caloriesText.split("\n\n")
+    caloriesPerElfValues = [list(convertLinesToListOfInts(caloriesPerElf)) for caloriesPerElf in caloriesPerElfStrings]
+    return caloriesPerElfValues
+
 def main():
-    print(bestElf(calories))
+    with(open("calories.txt", "r") as caloriesFile):
+        fullFileContent = caloriesFile.read()
+        print(bestElf(caloriesTextToList(fullFileContent)))
 
 if __name__ == "__main__":
     main()
